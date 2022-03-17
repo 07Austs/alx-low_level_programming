7c517bfe744b1aa14be9e683946c0292c31ffd8f
eyannoh <eyannohmmoja@gmail.com>
2022-03-17 04:03:03 +0300

    update

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * add -prints add two numbers
+ *@i: print int i
+ *@k: print int k
  * Return: Always 0.
  */
-int main(void)
-{
-	int n;
 
-	n = add(89, 9);
-	printf("%d\n", n);
-	return (0);
+int add(int i, int k)
+{
+return (i + k);
 }
